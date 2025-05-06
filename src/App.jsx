346b850736096017897bb38dc47ab38baf2ce058
MAMP29 <106500061+MAMP29@users.noreply.github.com>
2025-05-05 21:18:27 -0500

    Agregado los modulos para modalsdeduction y mi set de preguntas

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { setData as setDataLinkwords, setLoading as setLoadingLinkwords, setErro
 import { setData as setDataVerbs, setLoading as setLoadingVerbs, setError as setErrorVerbs } from "./store/verbs";
 import { setData as setDataAdjectives, setLoading as setLoadingAdjectives, setError as setErrorAdjectives } from "./store/adjectives";
 import { setData as setDataFirstConditional, setLoading as setLoadingFirstConditional, setError as setErrorFirstConditional } from "./store/firstconditional";
+import { setData as setDataModalsDeduction, setLoading as setLoadingModalsDeduction, setError as setErrorModalsDeduction } from "./store/modalsdeduction";
 import AppNavbar from './components/Navbar/AppNavbar';
 import Sidebar from './components/Sidebar/Sidebar';
 import { fetchDataHelper } from "./helpers/fetchDataHelper";
@@ -61,6 +62,15 @@ function App() {
       setErrorFirstConditional,
       `${import.meta.env.BASE_URL}firstconditional`
     );
+
+    // Fetch for `Modals Deduction`
+    fetchDataHelper(
+      dispatch,
+      setLoadingModalsDeduction,
+      setDataModalsDeduction,
+      setErrorModalsDeduction,
+      `${import.meta.env.BASE_URL}modalsdeduction`
+    )
     
   }, [dispatch]);
 
